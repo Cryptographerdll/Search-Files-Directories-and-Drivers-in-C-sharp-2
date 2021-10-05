f20849845acef458dfe4b62250aaa026feaa77fe
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-10-05 22:19:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Les_Fichiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string Fils = path + @"\Fichiers\Tert.txt";
+
+            bool existe = false;
+
+            existe = File.Exists(Fils);
+
+            if (!existe)
+            {
+                Console.WriteLine("Fichier existe pas ");
+                File.Create(Fils);
+            }
+            else
+                Console.WriteLine("le fichier est existe en folder");
+    
+            Console.ReadLine();
+        }
+    }
+}
